de79b91c6292a1b7f891434740cb6118fc48b83a
harsh mittal <harshmittal1750@gmail.com>
2022-08-04 16:02:50 +0530

    palindromic Number Pyramid

---
+++ palindromicNumberPyramid.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class palindromicNumberPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = n; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+        sc.close();
+    }
+}
