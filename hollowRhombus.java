8cccce5ab057b7f251aad9de371638798801b7d4
Harsh Mittal <harshmittal1750@gmail.com>
2022-08-06 13:14:47 +0530

    hollow rhombus

---
+++ hollowRhombus.java
@@ -0,0 +1,28 @@
+
+public class hollowRhombus {
+    public static void main(String[] args) {
+
+        int n = 4;
+        for (int i = 0; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                if (i == n || i == 0 || j == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            for (int j = n; j >= i; j--) {
+                if (j == 0 || j == 1 || j == i || i == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
